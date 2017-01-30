beeddad0ca1b90ca174345f1e69b3d87b4957317
Delton Ding <dsh0416@gmail.com>
2017-01-31 01:14:12 +0800

    Improve the entrance

--- auto.py
+++
@@ -1,2 +0,0 @@
-from iro.train import train
-train()
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+import sys
 from iro.download import download
 from iro.preload import preload
 from iro.train import train
 from iro.predictor import predict
 from iro.validator import validate
 
-method = input("1. Download\n2. Preload\n3. Train\n4. Predict\n5. Validate\nEnter Your Input: ")
-if method == '1':
-    download()
-elif method == '2':
-    preload()
-elif method == '3':
-    train()
-elif method == '4':
-    predict()
-elif method == '5':
-    validate()
-else:
-    print('No method matched')
-
-exit()
+if __name__ == '__main__':
+    args = sys.argv
+    if args[1] == 'download':
+        download()
+    elif args[1] == 'preload':
+        preload()
+    elif args[1] == 'train':
+        train()
+    elif args[1] == 'predict':
+        predict()
+    elif args[1] == 'validate':
+        validate()
+    else:
+        print('start with args: download, preload, train, predict, validate')
+    exit()
