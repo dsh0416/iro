0c3bdaaa4b4d32eab94bbb7c731775dbcdc96897
Delton Ding <dsh0416@gmail.com>
2017-02-01 19:44:17 +0800

    Improve network

--- iro/network.py
+++ iro/network.py
@@ -1,96 +1,102 @@
-from keras.models import Sequential
-from keras.layers import Convolution2D, GaussianNoise, BatchNormalization, MaxPooling2D, Flatten, Dense, Reshape
-from keras.callbacks import ReduceLROnPlateau
+from keras.models import Sequential, Model
+from keras.layers import Convolution2D, GaussianNoise, BatchNormalization, Flatten, Dense, Reshape
+from keras.layers import Input, MaxPooling2D, UpSampling2D, merge
+from keras.callbacks import EarlyStopping
 from iro.data import Data
+
+
+class Generator:
+    def __init__(self):
+        inputs = Input((128, 128, 3))
+        conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
+        conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+        conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
+        conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+        conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
+        conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+        conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
+        conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
+
+        up5 = merge([UpSampling2D(size=(2, 2))(conv4), conv3], mode='concat', concat_axis=3)
+        conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up5)
+        conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv5)
+
+        up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv2], mode='concat', concat_axis=3)
+        conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up6)
+        conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv6)
+
+        up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv1], mode='concat', concat_axis=3)
+        conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up7)
+        conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv7)
+
+        conv8 = Convolution2D(3, 1, 1, activation='sigmoid')(conv7)
+        self.network = Model(input=inputs, output=conv8)
+        self.network.compile(
+            optimizer='Nadam',
+            loss='mse',
+        )
+
+
+class Discriminator:
+    def __init__(self):
+        inputs = Input((128, 128, 3))
+        conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
+        conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+        conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
+        conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+        conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
+        conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+        conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
+        conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
+
+        up5 = merge([UpSampling2D(size=(2, 2))(conv4), conv3], mode='concat', concat_axis=3)
+        conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up5)
+        conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv5)
+
+        up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv2], mode='concat', concat_axis=3)
+        conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up6)
+        conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv6)
+
+        up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv1], mode='concat', concat_axis=3)
+        conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up7)
+        conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv7)
+
+        conv8 = Convolution2D(3, 1, 1, activation='relu')(conv7)
+        flatten1 = Flatten()(conv8)
+        dense1 = Dense(128 * 128 * 3, activation='relu')(flatten1)
+        dense2 = Dense(32 * 32 * 3, activation='relu')(dense1)
+        dense3 = Dense(4 * 4 * 3, activation='relu')(dense2)
+        dense4 = Dense(1, activation='sigmoid')(dense3)
+        self.network = Model(input=inputs, output=dense4)
+        self.network.compile(
+            optimizer='Nadam',
+            loss='mse',
+        )
 
 
 class GAN:
     def __init__(self):
         self.data = None
-        self.generator_network = Sequential([
-            Convolution2D(32, 3, 3,
-                          input_shape=(128, 128, 3),
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(2, 2)),
-            GaussianNoise(0.1),
-            Convolution2D(32, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(3, 3)),
-            BatchNormalization(),
-            Convolution2D(64, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(2, 2)),
-            Convolution2D(64, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(3, 3)),
-            BatchNormalization(),
-            Convolution2D(128, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(2, 2)),
-            Convolution2D(128, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(3, 3)),
-            BatchNormalization(),
-            Flatten(),
-            Dense(128 * 128 * 3, activation='sigmoid'),
-            Reshape((128, 128, 3)),
-        ], name='Generator')
-
-        self.discriminator_network = Sequential([
-            Convolution2D(32, 3, 3,
-                          input_shape=(128, 128, 3),
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(2, 2)),
-            GaussianNoise(0.1),
-            Convolution2D(32, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(3, 3)),
-            BatchNormalization(),
-            Convolution2D(64, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(2, 2)),
-            Convolution2D(64, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(3, 3)),
-            BatchNormalization(),
-            Convolution2D(128, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(2, 2)),
-            Convolution2D(128, 2, 2,
-                          activation='relu',
-                          border_mode='same',
-                          subsample=(3, 3)),
-            BatchNormalization(),
-            Flatten(),
-            Dense(128 * 128 * 3, activation='relu'),
-            Dense(1, activation='sigmoid')
-        ], name='Discriminator')
+        self.generator_network = Generator().network
+        self.discriminator_network = Discriminator().network
 
         self.gan_network = Sequential([
             self.generator_network,
             self.discriminator_network,
         ], name='GAN')
 
-        self.generator_network.compile(
-            optimizer='Nadam',
-            loss='mse',
-        )
-
-        self.discriminator_network.compile(
-            optimizer='Adam',
-            loss='mse',
-        )
         self.gan_network.compile(
             optimizer='Adam',
             loss='mse',
@@ -117,6 +123,7 @@ class GAN:
                 samples_per_epoch=samples,
                 nb_epoch=nb_epoch,
                 nb_worker=nb_worker,
+                callbacks=[EarlyStopping(monitor='loss', patience=5, verbose=0, mode='auto')]
             )
 
             print('Training Generator')
@@ -126,6 +133,7 @@ class GAN:
                 samples_per_epoch=samples,
                 nb_epoch=nb_epoch,
                 nb_worker=nb_worker,
+                callbacks=[EarlyStopping(monitor='loss', patience=5, verbose=0, mode='auto')]
             )
 
             self.discriminator_network.save_weights('./checkpoint/discriminator.new.hdf5')
